5c3e6d0a42f059ae2ed9f08b7e463efcc6ae3707
Dominic Wood <ddwood87@yahoo.com>
2022-11-17 22:55:51 -0600

    TestUserController post method is passing, need to write a few more tests. Added JsonToUserConverter

---
+++ src/main/java/catalog/beans/JsonToUserConverter.java
@@ -0,0 +1,26 @@
+package catalog.beans;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author dominicwood - ddwood2@dmacc.edu
+ * CIS175 - Fall 2022
+ * Nov 17, 2022
+ */
+@Component
+public class JsonToUserConverter implements Converter<String, User> {
+	@Override
+	public User convert(String json){
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			User user = mapper.readValue(json, User.class);
+			return user;
+		}catch(Exception e) {
+			return null;
+		}
+	}
+}
---
+++ src/main/java/catalog/controller/WebConfig.java
@@ -0,0 +1,20 @@
+package catalog.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import catalog.beans.JsonToUserConverter;
+
+/**
+ * @author dominicwood - ddwood2@dmacc.edu
+ * CIS175 - Fall 2022
+ * Nov 17, 2022
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addConverter(new JsonToUserConverter());
+	}
+}
